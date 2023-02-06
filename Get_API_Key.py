e960da7540c0a150734bcb438c0d031fd1eb71c2
Jaeyeong Choe <51697343+cjy3676@users.noreply.github.com>
2023-02-06 15:43:06 +0900

    "나만의 데이터 지도 만들기"
    오픈 API 신청하기

---
+++ Get_API_Key.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## 공공데이터 API 키 받기
+
+# ### 1. 공공데이터 포탈 가입하기
+#     http://data.seoul.go.kr
+#     
+# ### 2. 원하는 데이터 셋 찾기
+#     서울특별시 공공자전거 실시간 대여정보
+#     http://data.seoul.go.kr/dataList/OA-15493/A/1/datasetView.do
+#     
+# ### 3. 인증키 신청하기
+# ![screenshot](./image/API_page.png)
+# 
+# ### 4. 가입신청서 작성
+# ![screenshop](./image/Request_Api.png)
+#     
+#     
+# #### 5. 인증키 관리 : 나의화면 --> 인증키관리
+# ![screenshot](./image/mypage.png)
+# 
+# #### 6. 인증키 복사하기
+# ![screenshot](./image/apikey.png)
